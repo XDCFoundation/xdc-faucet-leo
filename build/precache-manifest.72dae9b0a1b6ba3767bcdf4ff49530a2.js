d0abb3d2d70ec5e480ab7f9dff385357c7137f2e
Sivaramakrishnan_r <sivaramakrishnan@leewayhertz.com>
2022-04-08 18:58:52 +0530

    build

---
+++ build/precache-manifest.72dae9b0a1b6ba3767bcdf4ff49530a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851b713866cc8cbf5098db3f64b4acdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b91b041088dd009b03a",
+    "url": "/static/css/main.d4b97416.chunk.css"
+  },
+  {
+    "revision": "9708028551ad7eb4aa91",
+    "url": "/static/js/2.32d2b9bf.chunk.js"
+  },
+  {
+    "revision": "6b91b041088dd009b03a",
+    "url": "/static/js/main.7cc3d095.chunk.js"
+  },
+  {
+    "revision": "8394a56d9b6f03322aa1",
+    "url": "/static/js/runtime~main.0522709a.js"
+  }
+]);
